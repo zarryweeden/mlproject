686eef999595027d86c01b735bf6e6a0e864144a
zarryweeden <zarryochola@gmailcom>
2025-02-19 14:12:46 +0300

    Model Trainer

--- src/utilis.py
+++ src/utilis.py
@@ -4,7 +4,28 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 import dill
+from sklearn.metrics import r2_score
 
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+
+            model.fit(X_train,y_train)
+            y_train_pred= model.predict(X_train)
+
+            y_test_pred= model.predict(X_test)
+            train_model_score= r2_score(y_train,  y_train_pred)
+
+            test_model_score= r2_score(y_test, y_train_pred)
+
+            report[list(models.keys())[i]]=train_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e,sys)    
 
 def save_object(file_path,obj):
     try:
