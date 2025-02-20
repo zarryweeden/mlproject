acf3a7601e500040d63912ba228aca2cf46a950f
zarryweeden <zarryochola@gmailcom>
2025-02-20 11:06:39 +0300

    Hyperparameter Training

--- src/utilis.py
+++ src/utilis.py
@@ -5,14 +5,21 @@ import pandas as pd
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
-
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+from sklearn.model_selection import GridSearchCV
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para= param[list(models.keys())[i]]
 
+            gs= GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+            #model.fit(X_train,y_train)
             y_train_pred= model.predict(X_train)
 
             y_test_pred= model.predict(X_test)
