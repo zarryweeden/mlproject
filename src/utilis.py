c5e277e803f56578e9b50d03ba86b6e580d2db68
zarryweeden <zarryochola@gmailcom>
2025-02-22 19:39:49 +0300

    Prediction

--- src/utilis.py
+++ src/utilis.py
@@ -6,6 +6,8 @@ from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
+
 def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
@@ -44,4 +46,13 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+
+    except Exception as e:
+        raise CustomException(e,sys)    
     
